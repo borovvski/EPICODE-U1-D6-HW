8f39b7b70100b852f77ab37477f5180d34f4f1c9
borovvski <brandonborovvski@gmail.com>
2022-07-04 14:46:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let firstName;
+firstName = "Brandon ";
+
+let secondName = "Lukasz ";
+
+let lastName = "Borowski";
+
+let myFullName = firstName + secondName + lastName;
+
+console.log("\nhello, my name is:", myFullName);
+
+let myAge = 28;
+
+let currentYear = 2022;
+
+let myYearOfBirth = currentYear - myAge;
+
+console.log("\nI was born in:", myYearOfBirth);
